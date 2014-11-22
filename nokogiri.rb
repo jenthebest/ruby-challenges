2b774e309cc47ee63b3a0cf7876f24a2bc368b1c
Jen Best <jenthebest@gmail.com>
2014-11-21 20:28:13 -0500

    Added nokogiri file

---
+++ nokogiri.rb
@@ -0,0 +1,12 @@
+require 'nokogiri'
+require 'open-uri'
+
+doc = Nokogiri::HTML(open('http://www.marthastewart.com/312598/brick-pressed-sandwich'))
+
+list = doc.css('.components-data').children
+
+list.each do |x|
+	puts x
+end
+
+#Happy dance because it worked! 
